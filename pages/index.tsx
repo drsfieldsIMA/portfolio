a2442faeb52aac9b69f7fe89408600cc6a7461ea
drsfieldsIMA <57874229+drsfieldsIMA@users.noreply.github.com>
2022-07-13 18:07:49 +0200

    design

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,12 +38,12 @@ function Home() {
 				<div className='collapse navbar-collapse' id='navbarNav'>
 					<ul className='navbar-nav'>
 						<li className='nav-item active'>
-							<a className='nav-link' href='index.html'>
+							<a className='nav-link' href='/'>
 								Home
 							</a>
 						</li>
 						<li className='nav-item'>
-							<a className='nav-link' href='design.html'>
+							<a className='nav-link' href='/design'>
 								Design Process
 							</a>
 						</li>
